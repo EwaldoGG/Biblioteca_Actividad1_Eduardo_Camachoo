cb19b37e3d16ef28163ef83b3cd35a9032ee925c
EwaldoGG <161513615+EwaldoGG@users.noreply.github.com>
2024-10-25 22:43:14 -0400

    Add files via upload

---
+++ Libreria Virtual.Eduardito/Library_Eduardo_Camacho_Console/Library_Eduardo_Camacho_Console/Librobl.vb
@@ -0,0 +1,32 @@
+﻿Public Class Librobl
+    Private _titulo As String
+    Private _autor As String
+    Private _isAvailable As Boolean
+
+    ' Constructor
+    Public Sub New(titulo As String, autor As String)
+        _titulo = titulo
+        _autor = autor
+        _isAvailable = True
+    End Sub
+
+    ' Métodos
+    Public Function Prestar() As String
+        If _isAvailable Then
+            _isAvailable = False
+            Return "Libro prestado: " & _titulo
+        Else
+            Return "El libro '" & _titulo & "' no está disponible."
+        End If
+    End Function
+
+    Public Function Devolver() As String
+        If Not _isAvailable Then
+            _isAvailable = True
+            Return "Libro devuelto: " & _titulo
+        Else
+            Return "El libro '" & _titulo & "' ya estaba disponible."
+        End If
+    End Function
+End Class
+
